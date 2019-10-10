52c47f62dabbd557916f0677a56acb64233effb1
dan6343 <dan6343@users.noreply.github.com>
2019-10-10 12:03:25 +0700

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Files\Coding\SiAODvEVM\Labs\Lab1\file.txt";
+            string[] numStr = new string[20];
+            int[] numInt;
+            int i = 0;
+            Queue q1 = new Queue();
+            Queue q2 = new Queue();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    line = sr.ReadLine();
+                    numStr = line.Split(' ');
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            numInt = new int[numStr.Length];
+            for (i = 0; i < numStr.Length; i++)
+            {
+                if (numStr[i] != "") { numInt[i] = Convert.ToInt32(numStr[i]); }
+            }
+            for (i = 0; i < numInt.Length; i++)
+            {
+                if (numInt[i] % 2 == 0)
+                {
+                    q1.Push(numInt[i]);
+                }
+                else
+                {
+                    q2.Push(numInt[i]);
+                }
+            }
+            Console.WriteLine("Первая очередь:");
+            while (!q1.IsEmpty())
+            {
+                Console.WriteLine(q1.Pop());
+            }
+            Console.WriteLine("Вторая очередь:");
+            while (!q2.IsEmpty())
+            {
+                Console.WriteLine(q2.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Queue
+    {
+        private int[] queue = new int[20];
+        private int t;
+        public Queue()
+        {
+            t = queue.Length;
+        }
+        public void Push(int a)
+        {
+            if (t < 0)
+            {
+                throw new OverflowException("Queue Overflow");
+            }
+            else
+            {
+                t--;
+                queue[t] = a;
+            }
+        }
+        public int Pop()
+        {
+            if (t > queue.Length)
+            {
+                throw new System.ArgumentException("Queue Empty");
+            }
+            else
+            {
+                int a = 0;
+                a = queue[t];
+                t++;
+                return a;
+            }
+        }
+        public int Top()
+        {
+            return queue[t];
+        }
+        public bool IsEmpty()
+        {
+            if (t == queue.Length) 
+            {
+                return true;
+            } 
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
